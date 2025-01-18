513e562195dcb6e09e8f90564b7e61a988d6f52e
Sarang Gaikwad <128568827+gaikwadsarang@users.noreply.github.com>
2025-01-18 19:35:29 +0530

    Add files via upload

---
+++ Calculator Using React/calculator/src/components/ButtonsContainer.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import styles from "./ButtonsContainer.module.css";
+
+function ButtonsContainer({ onButtonClick }) {
+  const buttonNames = [
+    "C",
+    "1",
+    "2",
+    "+",
+    "3",
+    "4",
+    "-",
+    "5",
+    "6",
+    "*",
+    "7",
+    "8",
+    "/",
+    "=",
+    "9",
+    "0",
+    ".",
+  ];
+  return (
+    <div>
+      <div className={styles.buttonsContainer}>
+        {buttonNames.map((buttonName) => (
+          <button
+            className={styles.button}
+            onClick={() => onButtonClick(buttonName)}
+          >
+            {buttonName}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default ButtonsContainer;
